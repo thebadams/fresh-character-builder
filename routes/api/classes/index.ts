3a7222e8b126c458cca283c9a88fba12c7a426a9
Brian Adams <bpeteradams@gmail.com>
2023-07-17 20:55:20 -0400

    API: Created a POST route for /api/classes/ that returns a result based upon the body passed with the request

--- routes/api/classes/index.ts
+++ routes/api/classes/index.ts
@@ -1,9 +1,23 @@
 import { Handlers } from "$fresh/server.ts";
 import IResults from "../../../types/IResults.ts";
 
+interface ClassBody {
+  name: string;
+}
 export const handler: Handlers<IResults> = {
-  GET(_req, _ctx) {
+  GET: (_req, _ctx) => {
     const data = "All Classes";
     return new Response(JSON.stringify({ results: data }));
+  },
+  POST: async (req, _ctx) => {
+    const payload = await req.json() as ClassBody;
+
+    const payloadData = payload.name;
+
+    const data = `Class ${payloadData} Created`;
+
+    return new Response(JSON.stringify({
+      results: data,
+    }));
   },
 };
